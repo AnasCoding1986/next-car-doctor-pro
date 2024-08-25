e28c3c0a5e2d034790536c5e518868097c1fbe94
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-08-25 19:48:27 +0600

    navbar completed

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,29 +1,30 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
+import { IoCart, IoSearch } from "react-icons/io5";
 
 const Navbar = () => {
 
     const navIteams = [
         {
-            title : "Home",
-            path : "/"
+            title: "Home",
+            path: "/"
         },
         {
-            title : "About",
-            path : "/about"
+            title: "About",
+            path: "/about"
         },
         {
-            title : "Services",
-            path : "/services"
+            title: "Services",
+            path: "/services"
         },
         {
-            title : "Blog",
-            path : "/blog"
+            title: "Blog",
+            path: "/blog"
         },
         {
-            title : "Contacts",
-            path : "/contacts"
+            title: "Contacts",
+            path: "/contacts"
         }
     ]
 
@@ -32,18 +33,24 @@ const Navbar = () => {
             <div className="navbar container mx-auto">
                 <div className="navbar-start">
                     <Link href={"/"}>
-                    <Image src="/assets/logo.svg" alt='logo' height={60} width={60}></Image>
+                        <Image src="/assets/logo.svg" alt='logo' height={60} width={60}></Image>
                     </Link>
                 </div>
                 <div className="navbar-center hidden lg:flex">
-                    <div>
+                    <div className='space-x-2'>
                         {
-                            navIteams.map(navIteam)
+                            navIteams.map((navIteam) => (
+                                <Link className='font-semibold hover:text-primary duration-300' key={navIteam.path} href={navIteam.path}>{navIteam.title}</Link>
+                            ))
                         }
                     </div>
                 </div>
                 <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    <div className='items-center space-x-5 flex'>
+                        <IoCart />
+                        <IoSearch />
+                        <a className="btn btn-outline btn-primary hover:text-white px-8">Appointment</a>
+                    </div>
                 </div>
             </div>
         </div>
