192013a8da03b0784cbe267d75ef810bc3701c25
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-08-25 18:23:20 +0600

    working with navitems

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/shared/Navbar";
+import Foter from "@/components/shared/Foter";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" data-theme="carDoctorTheme">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar></Navbar>
+        {children}
+        <Foter></Foter>
+      </body>
     </html>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,9 @@
-import Image from "next/image";
+import Homepage from "@/components/HomePage/Homepage";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>Hello Car Doctor</div>
-
-<button className="btn btn-primary text-white">Primary</button>
-
+    <main>
+      <Homepage></Homepage>
     </main>
   );
 }
