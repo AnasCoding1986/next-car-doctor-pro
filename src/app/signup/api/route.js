9a1f6eac3022cfb66cac208c7c03d0841c45176e
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-08-27 22:21:07 +0600

    bicript will start

---
+++ src/app/signup/api/route.js
@@ -0,0 +1,32 @@
+import { connectDB } from "@/lib/connectDB";
+
+export const POST = async (req) => {
+    const newUser = await req.json();
+    try {
+        const db = await connectDB();
+        const userCollection = db.collection('users');
+        
+        // Check if the user already exists
+        const exists = await userCollection.findOne({ email: newUser.email });
+        if (exists) {
+            return new Response(JSON.stringify({ message: "User already exists" }), {
+                status: 409, 
+                headers: { 'Content-Type': 'application/json' }
+            });
+        }
+        
+        // Insert the new user into the collection
+        await userCollection.insertOne(newUser);
+        return new Response(JSON.stringify({ message: "User created" }), {
+            status: 201, 
+            headers: { 'Content-Type': 'application/json' }
+        });
+
+    } catch (error) {
+        console.error("Error creating user:", error);
+        return new Response(JSON.stringify({ message: "Something went wrong", error: error.message }), {
+            status: 500, 
+            headers: { 'Content-Type': 'application/json' }
+        });
+    }
+};
