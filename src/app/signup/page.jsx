975c7e2aa443a63585d2d446e1679f75ac220761
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-08-27 11:50:49 +0600

    mongo db starting

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -1,9 +1,16 @@
+"use client";
 import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 import { FaFacebook, FaLinkedin, FaGoogle } from "react-icons/fa";
 
 const page = () => {
+
+    const handleSignUp = () => {
+        console.log("x");
+        
+    }
+
     return (
         <div className='container mx-auto py-10'>
             <div className='grid grid-cols-1 md:grid-cols-2 px-10'>
@@ -12,13 +19,19 @@ const page = () => {
                 </div>
                 <div className='flex items-center justify-center flex-col'>
                     <h2 className='text-center text-primary text-4xl font-semibold mb-10'>Sign Up</h2>
-                    <label htmlFor="name">Name</label> <br />
-                    <input type="text" placeholder="Name" name='name' className="input input-bordered w-full" /><br />
-                    <label htmlFor="email">Email</label> <br />
-                    <input type="text" placeholder="Name" name='email' className="input input-bordered w-full" /><br />
-                    <label htmlFor="password">Password</label> <br />
-                    <input type="password" placeholder="Enter Password" name='password' className="input input-bordered w-full" />
-                    <button className='btn btn-primary w-full mt-12'>Sign up</button>
+
+                    <form onSubmit={handleSignUp} action="">
+                        <label htmlFor="name">Name</label> <br />
+                        <input type="text" placeholder="Name" name='name' className="input input-bordered w-full" /><br />
+                        <label htmlFor="email">Email</label> <br />
+                        <input type="text" placeholder="Name" name='email' className="input input-bordered w-full" /><br />
+                        <label htmlFor="password">Password</label> <br />
+                        <input type="password" placeholder="Enter Password" name='password' className="input input-bordered w-full" />
+                        
+                        <button type='submit'  className='btn btn-primary w-full mt-12'>Sign up</button>
+                    </form>
+
+
                     <div className='my-5'>
                         <p>Or sign up with</p>
                     </div>
