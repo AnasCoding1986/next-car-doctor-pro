a58fa0835255d49bfc64a51e6c8904402075da94
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-08-31 11:58:19 +0600

    solve 1

--- src/app/services/api/getAll/route.js
+++ src/app/services/api/getAll/route.js
@@ -1,15 +1,12 @@
-import { connectDB } from "@/lib/connectDB";
+import { connectDB } from "@/lib/connectDB"
 
 export const GET = async () => {
-  try {
-    const db = await connectDB(); // Ensure the database connection is awaited
+    const db = await connectDB();
     const servicesCollection = db.collection('services');
-
-    const services = await servicesCollection.find().toArray();
-
-    return new Response(JSON.stringify({ services }));
-  } catch (error) {
-    console.error("Error getting services:", error);
-    return new Response(JSON.stringify({ message: 'Failed to get data' }), { status: 500 });
-  }
-};
+    try {
+        const services = await servicesCollection.find().toArray();
+        return Response.json({services})
+    } catch (error) {
+        console.log(error);       
+    }
+}
