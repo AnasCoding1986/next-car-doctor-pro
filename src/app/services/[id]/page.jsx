2096932ee6a2fc251ef8a99be46b47cb13f2358a
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-09-01 07:14:21 +0600

    service details started

--- src/app/services/[id]/page.jsx
+++ src/app/services/[id]/page.jsx
@@ -1,14 +1,30 @@
 import { getServicesDetails } from '@/services/getServices';
 import React from 'react';
 
-const page = async ({params}) => {
+const page = async ({ params }) => {
     const details = await getServicesDetails(params.id);
-    const {title,img,price,description,facility} = details.service;
-    console.log(title,img,price,description,facility);
-    
+    const { title, img, price, description, facility } = details.service;
+    console.log(title, img, price, description, facility);
+
     return (
         <div>
-            <h2>This is details</h2>
+            <div
+                className="hero min-h-screen"
+                style={{
+                    backgroundImage: "url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+                }}>
+                <div className="hero-overlay bg-opacity-60"></div>
+                <div className="hero-content text-neutral-content text-center">
+                    <div className="max-w-md">
+                        <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+                        <p className="mb-5">
+                            Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                            quasi. In deleniti eaque aut repudiandae et a id nisi.
+                        </p>
+                        <button className="btn btn-primary">Get Started</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
