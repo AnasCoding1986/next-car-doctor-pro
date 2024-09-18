3a2487f70240e98241c8784755c9fc228d26c006
Azizul Rabby Chowdhury <anassust.scw@gmail.com>
2024-09-18 11:01:55 +0600

    dhum

--- src/app/services/page.jsx
+++ src/app/services/page.jsx
@@ -6,9 +6,6 @@ const page = () => {
             <h2>This is services</h2>
             <p>New try with different strategy</p>
             <p>Trying with support</p>
-            <p>Lets move on</p>
-            <p>17/09/2024(1)</p>
-            <p>17/09/2024(2)</p>
             <p>18/09/2024(1)</p>
         </div>
     );
